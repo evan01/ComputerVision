6178f5e70a4caa64063c213873098b034c99ae2f
eknox <eknoxmobile@gmail.com>
2017-02-12 20:32:17 -0500

    Almost done!
    Signed-off-by: eknox <eknoxmobile@gmail.com>

--- Assignment2/FaceMatching.py
+++ Assignment2/FaceMatching.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from pictureLogger import imageLogger
 import numpy as np
 import warnings
+import os
 
 warnings.filterwarnings("ignore")
 
@@ -13,7 +14,11 @@ DEBUG = True
 
 
 def importCelebImages():
-    pass  # todo implement this method
+    """
+    This function will import all the images, and compute their descriptors
+    :return: an array of image descriptors
+    """
+    os
 
 
 def resizeImage(image, dbug=DEBUG):
@@ -37,7 +42,7 @@ def computeLBPDescriptor(im):
     First thing to do is to get the binary label for each pixel value,
     and while doing that, build our histogram
     '''
-    for i in tqdm(range(1, sz[0] - 1), desc="LBPDescriptor"):
+    for i in range(1, sz[0] - 1):
         for j in range(1, sz[1] - 1):
             window = im[i - 1:i + 2, j - 1:j + 2]
             thresh = im[i][j]
@@ -54,22 +59,25 @@ def computeLBPDescriptor(im):
             bin_val = binary.dot(2 ** np.arange(binary.size)[::-1])
             histogram[bin_val] += 1
 
-    print "done"
-
+    return histogram
 
 def breakImageApart(im):
     """
-    this function will break up the original image apart into 7x7 sections
+    this function will break up the original image apart into roughly equal 7x7 sections
     :param im:
     :return:
     """
+    segments = []
     # First get all of the vertical slices, should be around 18 pixels tall each
     horizontalSlices = np.array_split(im, 7)
 
     # Then for each vertical slice, horizontally split the arrays into segments...
-    verticalSlices = np.vsplit(horizontalSlices[0], 7)
-    print "done"
+    for i in horizontalSlices:
+        verticalSlices = np.array_split(i, 7, axis=1)
+        for slice in verticalSlices:
+            segments.append(slice)
 
+    return segments
 
 def main():
     """
@@ -77,13 +85,18 @@ def main():
     :return: The celebrity I look the most like
     """
 
-    # First thing is to import all the images and resize them
+    # First thing is to import all the images, resize them, and compute their descriptors
     images = importCelebImages()
 
+    # Then get our image, resize it and compute it's descriptor
     me_im = cv2.imread("./images/Rebecca1.jpg", 0)
     me_im = resizeImage(me_im)
-
     imSegments = breakImageApart(me_im)
+    me_features = [computeLBPDescriptor(i) for i in tqdm(imSegments, desc="Finding features")]
+
+    # Then compare our fDescriptor with all the others! Will return index of image we want
+    im = compareFeatures(me_features, all_features)
+    print "done"
     # lbp = computeLBPDescriptor(me_im)
 
 
